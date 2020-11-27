4d27f0d526bc98416d477fa7f52cb989ec7ac789
Sawyer Steffen <sawyerstefferr@gmail.com>
2020-11-27 14:50:11 -0600

    Got rid of redundant code

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -19,35 +19,39 @@
     $('#shoppingCart').on('click', function (e) {
         e.preventDefault();
         console.log("click worked")
-        var email = $('#CurrentUser').data('email');
-        console.log("this email: ", email)
+        //var email = $('#CurrentUser').data('email');
+        //console.log("this email: ", email)
+        getCartItems();
         $('#shoppingModal').modal();
     });
 
     //todo woking here
-    //function getCartItems() {
-    //    var id = $('#user').data('email');
-    //    console.log("this id: ", id);
-    //        $.getJSON({
-    //        url: "api/cartitem/show-cart-items/" + id ,
-    //        success: function (response, textStatus, jqXhr) {
-    //            //console.log(response);
-    //            $('#product_rows').html("");
-    //            for (var i = 0; i < response.length; i++) {
-    //                var css = response[i].discontinued ? " class=\"discontinued\"" : "";
-    //                var row = "<tr" + css + " data-id=\"" + response[i].productId + "\" data-name=\"" + response[i].productName + "\" data-price=\"" + response[i].unitPrice + "\">"
-    //                    + "<td>" + response[i].productName + "</td>"
-    //                    + "<td class=\"text-right\">$" + response[i].unitPrice.toFixed(2) + "</td>"
-    //                    + "<td class=\"text-right\">" + response[i].unitsInStock + "</td>"
-    //                    + "</tr>";
-    //                $('#product_rows').append(row);
-    //            }
-    //        },
-    //        error: function (jqXHR, textStatus, errorThrown) {
-    //            // log the error to the console
-    //            console.log("The following error occured: " + textStatus, errorThrown);
-    //        }
-    //});
+    function getCartItems() {
+        var email = $('#User').data('email');
+        console.log("this email: ", email);
+        $.getJSON({
+            url: "api/cartitem/show-cart-items/" + email,
+            success: function (response, textStatus, jqXhr) {
+                console.log("response: ", response);
+                //todo cartitemid, customer, customerid, product, productid, quantity
+
+                //todo figure out how to add info to the modal
+                //$('#product_rows').html("");
+                //for (var i = 0; i < response.length; i++) {
+                //    var row = "<tr" + css + " data-id=\"" + response[i].productId + "\" data-name=\"" + response[i].productName + "\" data-price=\"" + response[i].unitPrice + "\">"
+                //        + "<td>" + response[i].productName + "</td>"
+                //        + "<td class=\"text-right\">$" + response[i].unitPrice.toFixed(2) + "</td>"
+                //        + "<td class=\"text-right\">" + response[i].unitsInStock + "</td>"
+                //        + "</tr>";
+                //    $('#product_rows').append(row);
+                //}
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // log the error to the console
+                console.log("The following error occured: " + textStatus, errorThrown);
+            }
+        })
+    };
 
     $(document).on('keyup', function (e) {
         if (e.key === "Escape") {
